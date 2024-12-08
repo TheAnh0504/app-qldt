200d7b17251e732e886cde0ec82d61206ca7cdd4
TheAnh0504 <luonluonyeuem3@gmail.com>
2024-12-08 20:26:37 +0700

    add config firebase

--- app_qldt/lib/main.dart
+++ app_qldt/lib/main.dart
@@ -1,4 +1,5 @@
 // cấu hình firebase --> gửi thông báo đẩy trong ứng dụng
+import "package:app_qldt/view/pages/home_skeleton.dart";
 import "package:firebase_core/firebase_core.dart";
 import "package:firebase_messaging/firebase_messaging.dart";
 // phát triển ứng dụng
@@ -9,6 +10,7 @@ import "package:flutter/material.dart";
 import "package:flutter/services.dart" hide TextInput;
 // tải dữ liệu từ internet, lưu trữ cục bộ trên thiết bị người dùng
 import "package:flutter_downloader/flutter_downloader.dart";
+import "package:flutter_hooks/flutter_hooks.dart";
 // Tạo, quản lý thông báo cục bộ trong ứng dụng (lên lịch, hiển thị khi ko có mạng)
 import "package:flutter_local_notifications/flutter_local_notifications.dart";
 // tạo các đề cập @username trong hộp thoại hoặc comment
@@ -59,13 +61,29 @@ void main() async {
       ]);
     });
   }
+
   // Lấy và in ra token của thiết bị cho Firebase Cloud Messaging, cho phép thiết bị nhận thông báo push từ Firebase
-  // Token này thường được sử dụng để đăng ký thiết bị nhận thông báo
-  print(await FirebaseMessaging.instance.getToken());
+  String? token = await FirebaseMessaging.instance.getToken();
+  print("Firebase Token: $token");
   // Tạo một kênh thông báo mặc định với id và tên là "default", bắt buộc khi gửi
   // thông báo đến các thiết bị Android hiện đại (từ Android 8 trở lên)
-  await AndroidFlutterLocalNotificationsPlugin().createNotificationChannel(
-      const AndroidNotificationChannel("default", "default"));
+  // Tạo kênh thông báo cho Android
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'default', // id của kênh
+    'Default Notifications', // tên hiển thị
+    description: 'This channel is used for default notifications.',
+    importance: Importance.high,
+  );
+  // await AndroidFlutterLocalNotificationsPlugin().createNotificationChannel(
+  //     const AndroidNotificationChannel("default", "default"));
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  // Đăng ký hàm xử lý khi ứng dụng chạy ngầm
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   // run app
   // ProviderScope đóng vai trò là gốc quản lý tất cả các provider trong ứng dụng
@@ -77,6 +95,26 @@ void main() async {
   //     child: Test(),
   //   )
   // );
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Thông báo khi ứng dụng chạy ngầm: ${message.notification?.title}");
+  print("Thông báo khi ứng dụng chạy ngầm: ${message.data}");
+  // Lấy URL hình ảnh từ dữ liệu thông báo
+  String? imageUrl = message.data['imageUrl'];
+
+  // Kiểm tra và sửa URL nếu cần
+  if (imageUrl != null && imageUrl.contains('drive.google.com')) {
+    // Sửa URL Google Drive thành link tải xuống trực tiếp
+    imageUrl = convertDriveUrlToDirectDownload(imageUrl);
+    print("Sửa URL hình ảnh: $imageUrl");
+  }
+
+}
+
+String convertDriveUrlToDirectDownload(String url) {
+  final fileId = url.split('/d/')[1].split('/')[0];
+  return 'https://drive.google.com/uc?export=download&id=$fileId';
 }
 
 // test run 1 page
@@ -97,6 +135,47 @@ class app_qldt extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    useEffect(() {
+      // final subscription = FirebaseMessaging.onMessage.listen((message) {
+      //   print("Thông báo khi ứng dụng ở foreground: ${message.notification?.title}");
+      // });
+      // Lắng nghe thông báo khi ứng dụng ở foreground
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        print("Thông báo khi ứng dụng ở foreground: ${message.notification?.title}");
+        print("1: ${message.notification?.body}");
+        print("2: ${message.notification?.android}");
+        print("3: ${message.notification?.apple}");
+        print("4: ${message.notification?.bodyLocKey}");
+        print("5: ${message.notification?.bodyLocArgs}");
+        print("6: ${message.notification?.titleLocKey}");
+        print("7: ${message.notification?.titleLocArgs}");
+        print("8: ${message.notification?.web}");
+        print("Thông báo khi ứng dụng chạy ngầm: ${message.data}");
+        // Điều hướng tới HomeSkeleton khi nhận thông báo
+        // Navigator.of(context).push(MaterialPageRoute(
+        //   builder: (context) => const HomeSkeleton(
+        //     child: Center(
+        //       child: Text('Chuyển đến HomeSkeleton sau thông báo'),
+        //     ),
+        //   ),
+        // ));
+      });
+
+      // Xử lý khi người dùng nhấn vào thông báo
+      FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+        print("Người dùng nhấn vào thông báo: ${message.notification}");
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => const HomeSkeleton(
+            child: Center(
+              child: Text('Chuyển đến HomeSkeleton khi nhấn thông báo'),
+            ),
+          ),
+        ));
+      });
+
+      return null;
+      // return subscription.cancel;
+    }, []);
     return Portal(
       child: MaterialApp.router(
           theme: ThemeData(
